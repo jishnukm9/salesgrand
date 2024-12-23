2be532bcc073b7fa5a239a714813fb1af62d7882
jishnunexcrest <jishnu@nexcrestit.com>
2024-12-23 11:34:07 +0530

    service model customer types changes when customer model customer type change

--- core/signals.py
+++ core/signals.py
@@ -1,9 +1,16 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import CoASubAccounts, GeneralLedger
+from .models import CoASubAccounts, GeneralLedger,Customers,Service
 
 @receiver(post_save, sender=CoASubAccounts)
 def update_general_ledger_entries(sender, instance, **kwargs):
     if instance.head_root:
         # Update all GeneralLedger entries that reference this CoASubAccounts
         GeneralLedger.objects.filter(subledger=instance).update(ledger=instance.head_root)
+
+
+
+@receiver(post_save, sender=Customers)
+def update_service_entries(sender, instance, **kwargs):
+    if instance.customertype:
+        Service.objects.filter(customerid=instance.unique_id).update(customertype=instance.customertype)
